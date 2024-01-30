104c8a8bb347d9d1ea33038fa9fff948257b0f52
dam23-13 <dam23-13@c16x25.iescierva.net>
2024-01-30 09:27:18 +0100

    EDT2 Mostrar mi nombre en un mensaje

--- Ejemplo1.java
+++ Ejemplo1.java
@@ -1,5 +1,8 @@
 public class Ejemplo1 {
 	public static void main(String args[]) {
 		System.out.println("Hola Mundo");
+		String nombre = "Carlos";
+		
+		System.out.println("Mi nombre es: " + nombre);
 	}
 }
